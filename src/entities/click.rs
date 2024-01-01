e330182a78b28831c233bea9e7e47b644c78f28d
wbmcglaughlin <wmcg0001@student.monash.edu>
2024-01-01 18:49:46 +1100

    doc: added comments

--- src/entities/click.rs
+++ src/entities/click.rs
@@ -16,14 +16,17 @@ pub fn check_click_selection(
                 .viewport_to_world_2d(global_transform, position)
                 .unwrap();
 
-            for (transform, mut player, mut children) in player_entity.iter_mut() {
+            for (transform, mut player, children) in player_entity.iter_mut() {
+                // Iterate over the children, there should only be one currently.
                 for child in &children {
+                    // Get the query element, this will throw an error if it doesnt contain a
+                    // highlight, but there is only one.
                     if let Ok(mut vis) = highlight.get_mut(*child) {
                         if *vis != Visibility::Visible {
+                            // TODO: need to handle this better. Hard coded currently.
                             let distance_squared = (ray_pos.x - transform.translation.x).powf(2.0)
                                 + (ray_pos.y - transform.translation.y).powf(2.0);
 
-                            // TODO: need to handle this better. Hard coded currently.
                             if distance_squared < 9.0 {
                                 *vis = Visibility::Visible;
                             }
