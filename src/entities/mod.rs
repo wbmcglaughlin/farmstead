66d25a8011adfe44ec5325c1d031f77024e93164
wbmcglaughlin <wmcg0001@student.monash.edu>
2024-01-04 17:55:11 +1100

    Revert "refa: reduce duplicate lines with helper func"
    This reverts commit 03d3fafdad89dab5a986bdd0fb03d502f4d47666.

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -1,5 +1,4 @@
 pub mod click;
 pub mod material;
-pub mod plants;
 pub mod player;
 pub mod tool;
--- src/entities/plants.rs
+++
@@ -1,7 +0,0 @@
-use bevy::prelude::*;
-
-#[derive(Component)]
-pub struct GrowingStagesAnimationIndex {
-    pub start: u32,
-    pub end: u32,
-}
