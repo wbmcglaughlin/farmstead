c877555d66f38eb916e925fc8250b19039c7e7f1
wbmcglaughlin <wmcg0001@student.monash.edu>
2024-01-04 18:01:02 +1100

    feat: wheat!

--- src/map/tilemap.rs
+++ src/map/tilemap.rs
@@ -16,6 +16,9 @@ pub struct JobLayerTileMap;
 #[derive(Component)]
 pub struct WaterTileMap;
 
+#[derive(Component)]
+pub struct PlantTileLayer;
+
 pub(crate) fn generate_map(mut commands: Commands, asset_server: Res<AssetServer>) {
     // This should be constant.
     let map_size = TilemapSize { x: 128, y: 128 };
@@ -142,6 +145,40 @@ pub(crate) fn generate_map(mut commands: Commands, asset_server: Res<AssetServer
             ..Default::default()
         },
         JobLayerTileMap,
+    ));
+
+    let tilemap_entity = commands.spawn_empty().id();
+    let mut tile_storage = TileStorage::empty(map_size);
+    let texture_handle: Handle<Image> = asset_server.load("wheat.png");
+
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+
+            let tile_entity = commands
+                .spawn(TileBundle {
+                    position: tile_pos,
+                    tilemap_id: TilemapId(tilemap_entity),
+                    texture_index: TileTextureIndex(0),
+                    ..Default::default()
+                })
+                .id();
+            tile_storage.set(&tile_pos, tile_entity);
+        }
+    }
+
+    commands.entity(tilemap_entity).insert((
+        TilemapBundle {
+            grid_size,
+            map_type,
+            size: map_size,
+            storage: tile_storage,
+            texture: TilemapTexture::Single(texture_handle),
+            tile_size,
+            transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.5),
+            ..Default::default()
+        },
+        PlantTileLayer,
     ));
 }
 
