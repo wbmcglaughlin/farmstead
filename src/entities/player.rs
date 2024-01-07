3be8b3c9e88beb71b6630233320cfd3f1e87945a
wbmcglaughlin <wmcg0001@student.monash.edu>
2024-01-07 16:59:50 +1100

    (unrelated) refa: removed player countdown timer

--- src/entities/player.rs
+++ src/entities/player.rs
@@ -12,10 +12,6 @@ use crate::{
 use super::{hitbox::HitBox, plant::Plant, EntityTileStorage, TileEntityType};
 
 const PLAYER_SPEED: f32 = 30.0;
-pub const PLAYER_SPAWN_TIMER_COOLDOWN: f32 = 0.5;
-
-#[derive(Resource)]
-pub struct PlayerSpawnTimer(pub Timer);
 
 #[derive(Component)]
 pub struct Player {
